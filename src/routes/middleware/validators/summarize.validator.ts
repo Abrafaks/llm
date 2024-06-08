6dc7eb74baa98f883886b47daa7368da6e054ab6
Abrafaks <abrafaks@users.noreply.github.com>
2024-06-08 21:27:08 +0200

    Remove old validator

--- src/routes/middleware/validators/summarize.validator.ts
+++ src/routes/middleware/validators/summarize.validator.ts
@@ -1,10 +1,5 @@
 import { body, ValidationChain } from 'express-validator';
 
-const amountOfSentences = body('amountOfSentences')
-  .isInt({ min: 1, max: 25 })
-  .withMessage('Amount of sentences has to be an integer, ranging from 1 to 25')
-  .toInt();
-
 const min_length = body('minLength')
   .isInt({ min: 15, max: 50 })
   .withMessage('min_length has to be an integer, ranging from 15 to 50')
@@ -19,7 +14,7 @@ const url = body('url').isURL().withMessage('url has to be a valid URL');
 
 export class SummarizeValidator {
   public validateCreateSummary(): ValidationChain[] {
-    return [amountOfSentences, url, min_length, max_length];
+    return [url, min_length, max_length];
   }
 }
 
