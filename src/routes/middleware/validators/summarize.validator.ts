3e6a72885c731e0223cbffa3e8b07221817a5899
Abrafaks <abrafaks@users.noreply.github.com>
2024-06-08 18:09:52 +0200

    Add length constraints' validation

--- src/routes/middleware/validators/summarize.validator.ts
+++ src/routes/middleware/validators/summarize.validator.ts
@@ -4,11 +4,22 @@ const amountOfSentences = body('amountOfSentences')
   .isInt({ min: 1, max: 25 })
   .withMessage('Amount of sentences has to be an integer, ranging from 1 to 25')
   .toInt();
+
+const min_length = body('minLength')
+  .isInt({ min: 15, max: 50 })
+  .withMessage('min_length has to be an integer, ranging from 15 to 50')
+  .toInt();
+
+const max_length = body('maxLength')
+  .isInt({ min: 50, max: 100 })
+  .withMessage('max_length has to be an integer, ranging from 50 to 100')
+  .toInt();
+
 const url = body('url').isURL().withMessage('url has to be a valid URL');
 
 export class SummarizeValidator {
   public validateCreateSummary(): ValidationChain[] {
-    return [amountOfSentences, url];
+    return [amountOfSentences, url, min_length, max_length];
   }
 }
 
