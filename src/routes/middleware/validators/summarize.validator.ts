14aaa2c47b74d7470ec273e8e8c32855d6ceb7f3
Abrafaks <abrafaks@users.noreply.github.com>
2024-06-08 22:54:52 +0200

    Change body validation

--- src/routes/middleware/validators/summarize.validator.ts
+++ src/routes/middleware/validators/summarize.validator.ts
@@ -1,20 +1,27 @@
 import { body, ValidationChain } from 'express-validator';
 
-const min_length = body('minLength')
-  .isInt({ min: 15, max: 50 })
-  .withMessage('min_length has to be an integer, ranging from 15 to 50')
+const min_length = body('min_length')
+  .isInt({ min: 10, max: 200 })
+  .withMessage('min_length has to be an integer, ranging from 10 to 200')
   .toInt();
 
-const max_length = body('maxLength')
-  .isInt({ min: 50, max: 100 })
-  .withMessage('max_length has to be an integer, ranging from 50 to 100')
+const max_length = body('max_length')
+  .isInt({ min: 10, max: 200 })
+  .withMessage('max_length has to be an integer, ranging from 10 to 200')
   .toInt();
+
+const minLessThanMax = body().custom((value, { req }) => {
+  if (req.body.min_length >= req.body.max_length) {
+    throw new Error('min_length should be less than max_length');
+  }
+  return true;
+});
 
 const url = body('url').isURL().withMessage('url has to be a valid URL');
 
 export class SummarizeValidator {
   public validateCreateSummary(): ValidationChain[] {
-    return [url, min_length, max_length];
+    return [min_length, max_length, minLessThanMax, url];
   }
 }
 
